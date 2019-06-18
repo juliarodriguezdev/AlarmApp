3af1e09c58d1d83d5ac1c8867b86aadc942ddd22
julia rodriguez <juliarodriguez@julia-rodriguezs-MacBook-Pro.local>
2019-06-18 15:33:42 -0600

    Added Persistent Data

--- AlarmApp/ModelController/AlarmController.swift
+++ AlarmApp/ModelController/AlarmController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class AlarmController {
     
+    init() {
+        loadFromPersistentStore()
+    }
     // Singleton
     
     static let sharedInstance = AlarmController()
@@ -22,25 +25,62 @@ class AlarmController {
         let secondAlarm = Alarm(name: "Sat Lesson Alarm", fireDate: Date())
         return [firstAlarm, secondAlarm]
     }
-    
+    // Create (CRUD)
     func addAlarm(fireDate: Date, name: String, enabled: Bool) {
         
         let newAlarm = Alarm(name: name, enabled: enabled, uuid: "will be used later", fireDate: fireDate)
         
         alarms.append(newAlarm)
+        saveToPersistentStorage()
+        
     }
-    
+    // Update (CRUD)
     func update(alarm: Alarm, fireDate: Date, name: String, enabled: Bool) {
         alarm.fireDate = fireDate
         alarm.name = name
         alarm.enabled = enabled
+        
+        saveToPersistentStorage()
     }
-    
+    // Delete (CRUD)
     func delete(alarm: Alarm) {
         guard let index = alarms.firstIndex(of: alarm) else { return }
         alarms.remove(at: index)
+        
+        saveToPersistentStorage()
     }
     func toggleEnabled(for alarm: Alarm) {
         alarm.enabled = !alarm.enabled
+    }
+    // Read (CRUD)
+    private func fireURL() -> URL {
+        let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let fileName = "alarm.json"
+        let documentsDirectoryURL = urls[0].appendingPathComponent(fileName)
+        return documentsDirectoryURL
+    }
+    
+    private func saveToPersistentStorage() {
+        let jsonEncoder = JSONEncoder()
+        
+        do {
+            let jsonAlarm = try jsonEncoder.encode(alarms)
+            
+            try jsonAlarm.write(to: fireURL())
+        } catch let encodingError {
+            print("There was an error saving the data as JSON! \(encodingError.localizedDescription)")
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        let jsonDecoder = JSONDecoder()
+        do {
+            let jsonData = try Data(contentsOf: fireURL())
+            let decodedAlarms = try jsonDecoder.decode([Alarm].self, from: jsonData)
+            alarms = decodedAlarms
+        } catch let decodingError {
+            print("There was an error decoding!! \(decodingError.localizedDescription)")
+        }
+        
     }
 }
